f992ef864e689ef54c236eda73791a651013a10d
fyelci <fyelci@gmail.com>
2019-11-10 22:26:41 +0000

    Implemented add message mutation

--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -6,8 +6,13 @@ const resolvers = {
   URL: URLResolver,
 
   Chat: {
+    messages(chat: any) {
+      return messages.filter(m => chat.messages.includes(m.id));
+    },
+
     lastMessage(chat: any) {
-      return messages.find(m => m.id === chat.lastMessage);
+      const lastMessage = chat.messages[chat.messages.length - 1];
+      return messages.find(m => m.id === lastMessage);
     },
   },
 
@@ -15,7 +20,36 @@ const resolvers = {
     chats() {
       return chats;
     },
+
+    chat(root: any, { chatId }: any) {
+      return chats.find(c => c.id === chatId);
+    },
   },
+  Mutation: {
+    addMessage(root: any, { chatId, content }: any) {
+      const chatIndex = chats.findIndex(c => c.id === chatId);
+
+      if (chatIndex === -1) return null;
+
+      const chat = chats[chatIndex];
+      const messagesIds = messages.map(currentMessage => Number(currentMessage.id));
+      const messageId = String(Math.max(...messagesIds) + 1);
+
+      const message = {
+        id: messageId,
+        createdAt: new Date(),
+        content,
+      };
+      messages.push(message);
+      chat.messages.push(messageId);
+      
+      // The chat will appear at the top of the ChatsList component
+      chats.splice(chatIndex, 1);
+      chats.unshift(chat);
+      return message;
+    },
+  },
+
 };
 
 export default resolvers;
